b90ddc2467fc3d113827317b686cd75deac88404
unknown <e2017139@cmaisonneuve.qc.ca>
2024-12-11 23:38:31 -0500

    version finale tu TP2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 //déclaration des objets qui pointe sur les éléments de la page
 let oMenuHTML = document.querySelector("#menualt");
+let oaSectionHTML = document.querySelectorAll("#menualt  a");
+let oaSectionTopHTML = document.querySelectorAll("#menutop  a");
+console.log(oaSectionHTML, oaSectionTopHTML);
 //fonction qui verifie la valeur de scrolly pour faire apparaitre et disparaitre le menu burger
 function testScroll(){
     if(window.scrollY>900){
